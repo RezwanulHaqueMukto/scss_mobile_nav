1182094b5c8d7b1a4d5e254b84fe868883bbeb88
REZWANUL1 <74784389+REZWANUL1@users.noreply.github.com>
2023-03-17 01:34:09 +0600

    adding scss mobile nav files

---
+++ main.js
@@ -0,0 +1,22 @@
+const nav_btn = document.querySelector(".mobile_nav #nav .nav_btn");
+const main_menu = document.querySelector(".mobile_nav .main_menu");
+const nav__items = document.querySelectorAll(
+	"mobile_nav .main_menu .nav__item"
+);
+
+nav__items.forEach((items) => {
+	items.addEventListener("click", function () {
+		alert("hi");
+	});
+});
+function add_nav_btn_border() {
+	nav_btn.classList.toggle("active");
+	main_menu.classList.toggle("active");
+	nav_btn.classList.toggle("nav_animation");
+	if (!nav_btn.classList.contains("nav_animation")) {
+		nav_btn.classList.add("remove_animation");
+	} else {
+		nav_btn.classList.remove("remove_animation");
+	}
+}
+nav_btn.addEventListener("click", add_nav_btn_border);
